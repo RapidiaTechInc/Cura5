b8d76adeb9ececed596645d1f9869078534a651c
thecampbells384 <thecampbells384@gmail.com>
2023-05-17 10:42:24 -0700

    changed label for support modifier to support modifier

--- plugins/SupportEraser/__init__.py
+++ plugins/SupportEraser/__init__.py
@@ -4,17 +4,22 @@
 from . import SupportEraser
 
 from UM.i18n import i18nCatalog
+
 i18n_catalog = i18nCatalog("cura")
+
 
 def getMetaData():
     return {
         "tool": {
-            "name": i18n_catalog.i18nc("@label", "Support Blocker"),
-            "description": i18n_catalog.i18nc("@info:tooltip", "Create a volume in which supports are not printed."),
+            "name": i18n_catalog.i18nc("@label", "Support Modifier"),
+            "description": i18n_catalog.i18nc(
+                "@info:tooltip", "Create a volume in which supports are not printed."
+            ),
             "icon": "SupportBlocker",
-            "weight": 4
+            "weight": 4,
         }
     }
 
+
 def register(app):
-    return { "tool": SupportEraser.SupportEraser() }
+    return {"tool": SupportEraser.SupportEraser()}
